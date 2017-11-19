7282face7d2fe4d9a6cfca87753a6c47740fb27a
Guillaume Rossignol <guillaume.rossignol@wizacha.com>
2017-11-19 21:19:17 +0100

    add transplant, recolte and preparation planche

--- calendrier.py
+++ calendrier.py
@@ -10,22 +10,37 @@ def calendrier():
     feuilleDate = getSheetByName(document, "Plan de jardin")
     feuilleSemi = getSheetByName(document, "Calendrier semis")
     feuilleCommande = getSheetByName(document, "Calendrier commandes")
+    feuillePrepaPlanche = getSheetByName(document, "Calendrier prépa planches")
+    feuilleTransplant = getSheetByName(document, "Calendrier transplant")
+    feuilleRecolte = getSheetByName(document, "Calendrier récolte")
 
     colonneSemi = 5
     colonneCommande = 4
-    #feuilleSemi = feuilles.getByName("Calendrier Semi")
+    colonnePrepaPlanche = 6
+    colonneTransplant = 7
+    colonneRecolte = 8
+
     calendrierSemi = {}
     calendrierCommande = {}
+    calendrierPrepaPlanche = {}
+    calendrierTransplant = {}
+    calendrierRecolte = {}
     
     ligneLue = 1
     #PArcours du plan de jardin pour renseigner les differents dictionnaires
     while feuilleDate.getCellByPosition(0, ligneLue).String != "" :
         remplirCalendrier(feuilleDate, calendrierSemi,colonneSemi, ligneLue)
         remplirCalendrier(feuilleDate, calendrierCommande,colonneCommande, ligneLue)
+        remplirCalendrier(feuilleDate, calendrierPrepaPlanche,colonnePrepaPlanche, ligneLue)
+        remplirCalendrier(feuilleDate, calendrierTransplant,colonneTransplant, ligneLue)
+        remplirCalendrier(feuilleDate, calendrierRecolte,colonneRecolte, ligneLue)
         ligneLue += 1
     #Remplissage des feuilles
     remplirFeuille(feuilleSemi,calendrierSemi)
     remplirFeuille(feuilleCommande,calendrierCommande)
+    remplirFeuille(feuillePrepaPlanche,calendrierPrepaPlanche)
+    remplirFeuille(feuilleTransplant,calendrierTransplant)
+    remplirFeuille(feuilleRecolte,calendrierRecolte)
 
 def getSheetByName(document, name):
     if document.Sheets.hasByName(name):
