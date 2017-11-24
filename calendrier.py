2ff77865ef23e315010e534f47fc167d33f4276f
Guillaume Rossignol <guillaume.rossignol@wizacha.com>
2017-11-24 15:32:33 +0100

    Utilisation de classe et gestion un peu plus générique

--- calendrier.py
+++ calendrier.py
@@ -14,33 +14,31 @@ def calendrier():
     document = XSCRIPTCONTEXT.getDocument()
     # prépare le travail sur les feuilles
     feuilleDate = getSheetByName(document, "Plan de jardin")
+    feuilleLegume = getSheetByName(document, 'Legumes')
     feuilleSemi = getSheetByName(document, "Calendrier semis")
     feuilleCommande = getSheetByName(document, "Calendrier commandes")
     feuillePrepaPlanche = getSheetByName(document, "Calendrier prépa planches")
     feuilleTransplant = getSheetByName(document, "Calendrier transplant")
     feuilleRecolte = getSheetByName(document, "Calendrier récolte")
 
-    colonneSemi = 5
-    colonneCommande = 4
-    colonnePrepaPlanche = 6
-    colonneTransplant = 7
-    colonneRecolte = 8
-
     calendrierSemi = {}
     calendrierCommande = {}
     calendrierPrepaPlanche = {}
     calendrierTransplant = {}
     calendrierRecolte = {}
-    
-    ligneLue = 1
-    #PArcours du plan de jardin pour renseigner les differents dictionnaires
-    while feuilleDate.getCellByPosition(0, ligneLue).String != "" :
-        remplirCalendrier(feuilleDate, calendrierSemi,colonneSemi, ligneLue)
-        remplirCalendrier(feuilleDate, calendrierCommande,colonneCommande, ligneLue)
-        remplirCalendrier(feuilleDate, calendrierPrepaPlanche,colonnePrepaPlanche, ligneLue)
-        remplirCalendrier(feuilleDate, calendrierTransplant,colonneTransplant, ligneLue)
-        remplirCalendrier(feuilleDate, calendrierRecolte,colonneRecolte, ligneLue)
-        ligneLue += 1
+
+    legumes = dictionnaireLegumes(feuilleLegume)
+    planches = listePlanche(feuilleDate, legumes)
+
+
+
+    for planche in planches:
+        remplirCalendrier(planche, calendrierSemi, "semi")
+        remplirCalendrier(planche, calendrierCommande, "commande")
+        remplirCalendrier(planche, calendrierPrepaPlanche, "preparation")
+        remplirCalendrier(planche, calendrierTransplant, "transplant")
+        remplirCalendrier(planche, calendrierRecolte, "premiereRecolte")
+
     #Remplissage des feuilles
     remplirFeuille(feuilleSemi,calendrierSemi)
     remplirFeuille(feuilleCommande,calendrierCommande)
@@ -53,21 +51,26 @@ def getSheetByName(document, name):
         return document.Sheets.getByName(name)
     document.Sheets.insertNewByName(name,0)
 
-def remplirCalendrier(feuille, calendrier, colonne, ligne):
-    date = feuille.getCellByPosition(colonne, ligne).Value
+def remplirCalendrier(planche, calendrier, colonne):
+    date = getattr(planche, colonne)
     #Petit workaround pour créer les listes ou les appends dans les dictionnaires
     if date > 0:
         if date in calendrier:
-            calendrier[date].append(feuille.getCellByPosition(colonneLegume, ligne).String)
+            calendrier[date].append(planche)
         else:
-            calendrier[date] = [feuille.getCellByPosition(colonneLegume, ligne).String]    
+            calendrier[date] = [planche]    
 
 def remplirFeuille(feuille, calendrier):
     feuille.getCellRangeByName("A2:B365").clearContents(7)
     line = 1
     for key in sorted(calendrier.keys()):
         feuille.getCellByPosition(0, line).Value = key
-        feuille.getCellByPosition(1, line).String = " | ".join(calendrier[key])
+        value = ""
+        def names(l):
+            for planche in l:
+                yield planche.legume.nom
+
+        feuille.getCellByPosition(1, line).String = " | ".join(names(calendrier[key]))
         line +=1
 
 class Legume:
@@ -89,7 +92,29 @@ class Legume:
         self.espacement = espacement
         self.quantitePlanche = quantitePlanche
 
-
+class Planche:
+    def __init__(self,\
+                 legume,\
+                 bloc,\
+                 planche,\
+                 iteration,\
+                 commande,\
+                 semi,\
+                 preparation,\
+                 transplant,\
+                 premiereRecolte):
+        self.legume = legume
+        self.bloc = bloc
+        self.planche = planche
+        self.iteration = iteration
+        self.commande = float(commande)
+        self.semi = float(semi)
+        self.preparation = float(preparation)
+        self.transplant = float(transplant)
+        self.premiereRecolte = float(premiereRecolte)
+    def getId(self):
+        return self.bloc+'-'+self.planche+'-'+self.preparation
+    
 def standardisationNom(nom):
     return sub('[^a-z]', '', nom.lower())
 
@@ -122,6 +147,43 @@ def dictionnaireLegumes(feuille):
                     )
         ligne += 1
     return dictionnaire
+
+def listePlanche(feuille, dictionnaireLegume):
+    #configuration de la feuille
+    colonneBloc = 0
+    colonnePlanche = 1
+    colonneIteration = 2
+    colonneNom = 3
+    colonneCommande = 4
+    colonneSemi = 5
+    colonnePreparation = 6
+    colonneTransplant = 7
+    colonnePremiereRecolte = 8
+
+    #Parcours de la feuille et génération du bouzin
+    planches = []
+
+    ligne = 1
+    while feuille.getCellByPosition(colonneBloc, ligne).String != "":
+        nomLegume = feuille.getCellByPosition(colonneNom, ligne).String
+        if standardisationNom(nomLegume) in dictionnaireLegume:
+            legume = dictionnaireLegume[standardisationNom(nomLegume)]
+        else:
+            legume = Legume(nomLegume,0,0,0,0,0,0,0)
+
+        planches.append(Planche(legume,
+            feuille.getCellByPosition(colonneBloc, ligne).String,
+            feuille.getCellByPosition(colonnePlanche, ligne).String,
+            feuille.getCellByPosition(colonneIteration, ligne).String,
+            feuille.getCellByPosition(colonneCommande, ligne).Value,
+            feuille.getCellByPosition(colonneSemi, ligne).Value,
+            feuille.getCellByPosition(colonnePreparation, ligne).Value,
+            feuille.getCellByPosition(colonneTransplant, ligne).Value,
+            feuille.getCellByPosition(colonnePremiereRecolte, ligne).Value
+                                ))
+
+        ligne += 1
+    return planches
 
     
         
